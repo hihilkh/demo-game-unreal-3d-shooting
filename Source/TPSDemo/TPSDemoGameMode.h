99587d9a2e5189c0f51e39c3991cc278037076fa
hihilkh <hihilkh@gmail.com>
2023-10-16 00:50:16 +0900

    新規プロジェクトを作成（Third Person テンプレート）

---
+++ Source/TPSDemo/TPSDemoGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TPSDemoGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATPSDemoGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATPSDemoGameMode();
+};
+
+
+
